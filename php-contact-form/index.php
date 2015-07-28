b63468f4466db5f530b947b6d437ecc4a35721f3
Jennifer <jennifergapay@gmail.com>
2015-07-27 23:24:54 -0400

    Completed PHP contact form

--- php-contact-form/index.php
+++ php-contact-form/index.php
@@ -19,18 +19,31 @@
       $error.="<br />Please enter your comment.";
     }
 
-    if ($_POST['name']!="" AND !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) { 
-        $error.="<br />Please enter a valid email address"
-    } 
+    if ($_POST['email']!="" AND !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) { 
+        $error.="<br />Please enter a valid email address";
+    }
 
     if ($error) {
 
       $result='<div class="alert alert-danger"><strong>There were error(s) in the form:</strong>'.$error.'</div>';
 
+    } else {
+
+      if (mail("jennifergapay@gmail.com", "Comment from website", "Name: ".$_POST['name']."
+
+        Email: ".$_POST['email']."
+
+        Comment: ".$_POST['comment'])) {
+
+          $result='<div class="alert alert-success"><strong>Thank You.</strong> I\'ll get back to you as soon as I can!</div>';
+
+      } else {
+
+          $result='<div class="alert alert-danger">Sorry, there was an error sending your message.  Please let me know <a href="http://jennifergapay.com/#get-in-touch">here</a>.</div>';
+
+      }
+
     }
-
-    
-
 
   }
 
@@ -41,13 +54,12 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>PHP email form</title>
-  <meta http-equiv="Content-type" content="text/html; charset=utf-8"
-  <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-
-  <!-- Optional theme -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>PHP email form | Jennifer's Sandbox</title>
+  <link href="..css/bootstrap.css" rel="stylesheet">
+  <link href="..css/custom.css" rel="stylesheet">
+  <link href='http://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet' type='text/css'>
 
   <style>
 
@@ -74,48 +86,85 @@
 </head>
 <body>
 
+<!-- =============== NAVBAR ===================-->
+
+  <div class="navbar navbar-default">
     <div class="container">
-
-      <div class="row">
-
-        <div class="col-md-6 col-md-offset-3 emailForm">
-
-          <h1>My email form</h1>
-
-          <?php echo $result; ?>
-
-          <p class="lead">Please get in touch - I'll get back to you as soon as I can.</lead>
-
-          <form method="post" id="myForm">
-
-            <div class="form-group">
-
-              <label for="name">Your Name</label>
-              <input type="text" name="name" class="form-control" placeholder="Your Name"/>
-
-            </div>
-
-            <div class="form-group">
-
-              <label for="email">Your Email</label>
-              <input type="email" name="email" class="form-control" placeholder="Your Email"/>
-
-            </div>
-
-            <div class="form-group">
-
-              <label for="comment">Your Comment</label>
-              <textarea class="form-control" name="comment"></textarea> 
-
-            </div>
-
-            <input type="submit" name="submit" class="btn btn-success btn-lg" value="Submit" />
-
-          </form>
-
+      <div class="navbar-header">
+        <a href="http://jennifergapay.com/" class="navbar-brand">home</a>
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
       </div>
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li><a href="http://jennifergapay.com/#projects">projects</a></li>
+          <li class="active"><a href="http://jennifergapay.com/sandbox">sandbox</a></li>
+          <li><a href="http://jennifergapay.com/#skills">skills</a></li>
+          <li><a href="http://jennifergapay.com/#explore">explore</a></li>
+          <li><a href="http://jennifergapay.com/#get-in-touch">get in touch</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+<!-- =============== HEADER ===================-->
+
+  <div class="page-header text-center">
+    <h1>A PHP contact form <small>A little experiment with PHP.</small> <a class="btn btn-default" href="https://github.com/jgypsygrrl/Sandbox/tree/master/php-contact-form" target="_blank" role="button">View Code &raquo;</a></h1>
+  </div>
+
+  <!-- container wrapper --> 
+
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-md-6 col-md-offset-3 emailForm">
+
+        <h1>Say Hello!</h1>
+
+        <?php echo $result; ?>
+
+        <p class="lead">Please get in touch - I'll get back to you as soon as I can.</lead>
+
+        <form method="post" id="myForm">
+
+          <div class="form-group">
+
+            <label for="name">Your Name</label>
+            <input type="text" name="name" class="form-control" placeholder="Your Name"/>
+
+          </div>
+
+          <div class="form-group">
+
+            <label for="email">Your Email</label>
+            <input type="email" name="email" class="form-control" placeholder="Your Email"/>
+
+          </div>
+
+          <div class="form-group">
+
+            <label for="comment">Your Comment</label>
+            <textarea class="form-control" name="comment"></textarea> 
+
+          </div>
+
+          <input type="submit" name="submit" class="btn btn-success btn-lg" value="Submit" />
+
+        </form>
 
     </div>
+
+  </div>
 
 
 
