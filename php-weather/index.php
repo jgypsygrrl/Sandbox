484f434b91ce6af07197031a98ae40039936b495
Jennifer <jennifergapay@gmail.com>
2015-07-28 12:28:12 -0400

    php-weather: complete frontend with bootstrap, added php.ini

--- php-weather/index.php
+++ php-weather/index.php
@@ -4,7 +4,6 @@
   <meta charset="UTF-8">
   <title>weatherApp</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
   <style>
 
     html, body{
@@ -16,19 +15,55 @@
       width:100%;
       height:100%;
       background-size:cover;
+      background-position:center;
 
     }
+
+    #details {
+      margin-top:125px;
+      text-align:center;
+    }
+
+    #details h1 {
+      font-size:300%;
+    }
+
+    #city {
+      margin:0 auto;
+      width:75%;
+    }
+
+    p {
+      padding-top: 5px;
+      padding-bottom: 5px;
+    }
+
   </style>
 </head>
 <body>
 
-  <div class="container contentContainer topContainer">
+  <div class="container contentContainer">
 
     <div class="row">
 
-      <div class="col-md-6 col-md-offset-3">
+      <div class="col-md-6 col-md-offset-3" id="details">
+
+        <h1 class="title">weatherApp</h1>
+
+          <p class="lead">Curious about the weather today?</p>
+
+          <form>
+
+            <div class="form-group">
+
+              <input type="text" class="form-control" placeholder="Type your city here.  ex: New York, Paris, London" name="city" id="city" />
+
+            </div>
+
+              <button class="btn btn-warning btn-lg">Shorts or Scarf?</button>
 
 
+          </form>
 
       </div>
 
