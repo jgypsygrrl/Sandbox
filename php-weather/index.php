8087257af5a4b0b5b6be3139bc8f9b7a15189c97
Jennifer <jennifergapay@gmail.com>
2015-07-28 09:58:52 -0400

    start PHP weather app

---
+++ php-weather/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>weatherApp</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+  <style>
+
+    html, body{
+      height:100%;
+    }
+
+    .container {
+      background-image:url("./images/background.jpg");
+      width:100%;
+      height:100%;
+      background-size:cover;
+
+    }
+  </style>
+</head>
+<body>
+
+  <div class="container contentContainer topContainer">
+
+    <div class="row">
+
+      <div class="col-md-6 col-md-offset-3">
+
+
+
+      </div>
+
+
+    </div>
+
+  </div>
+  
+
+<!-- ========== SCRIPTS ========== -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+</html>
