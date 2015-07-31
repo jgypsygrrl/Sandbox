fc5874b9f4d6ef8ce6df47eda6094e8c72e012ec
Jennifer <jennifergapay@gmail.com>
2015-07-30 21:58:02 -0400

    Add validation to phpJournal

--- php-journal/index.php
+++ php-journal/index.php
@@ -3,21 +3,22 @@
   if ($_POST['submit']) {
 
     //checks email validation
-    if (!$_POST['email']) $error.="Please enter your email";
-        else filter_var !($_POST['email'], FILTER_VALIDATE_EMAIL) $error.="Please enter a valid email address";
+    if (!$_POST['email']) $error.="<br />Please enter your email";
+        else if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) $error.="<br />Please enter a valid email address";
 
-    //checks password is more than 8 characters w/1 capital letter
-    if (!_POST['password']) $error.="Please enter your password";
+    //checks password is more than 8 characters
+    if (!$_POST['password']) $error.="<br />Please enter your password";
     else {
-      if (strlen($_POST['password'])<8) $error.="Please enter a password with at least 8 characters";
-      if (preg_match('`[A-Z)`', $_POST['password'])) $error.="Pleae include at least one capital letter in your password.";
+      if (strlen($_POST['password'])<8) $error.="<br />Please enter a password with at least 8 characters";
+      if (!preg_match('`[A-Z]`', $_POST['password'])) $error.="<br />Pleae include at least one capital letter in your password.";
     }
+    if ($error) echo "There were error(s) in your signup details:".$error;
 
   }
 
 ?>
 
-<form>
+<form method="post">
 
   <input type="email" name="email" id="email" />
   <input type="password" name="password" />
