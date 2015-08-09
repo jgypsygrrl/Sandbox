387516897e5ca3973f9723a5427f986ad03e7c6b
Jennifer <jennifergapay@gmail.com>
2015-08-09 07:40:13 -0400

    Move sandbox to Main, fixed nav on child pages, added 301 redirect

--- php-contact-form/index.php
+++ php-contact-form/index.php
@@ -56,7 +56,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>PHP email form | Jennifer's Sandbox</title>
+  <title>PHP email form | Jennifer's Sandblog</title>
   <link href="../css/bootstrap.css" rel="stylesheet">
   <link href="../css/custom.css" rel="stylesheet">
   <link href='http://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet' type='text/css'>
@@ -112,7 +112,7 @@
       <div class="collapse navbar-collapse">
         <ul class="nav navbar-nav">
           <li><a href="http://jennifergapay.com/#projects">projects</a></li>
-          <li class="active"><a href="http://jennifergapay.com/sandbox">sandbox</a></li>
+          <li class="active"><a href="http://www.jennifergapay.com/sandblog/">sandblog</a></li>
           <li><a href="http://jennifergapay.com/#skills">skills</a></li>
           <li><a href="http://jennifergapay.com/#explore">explore</a></li>
           <li><a href="http://jennifergapay.com/#get-in-touch">get in touch</a></li>
