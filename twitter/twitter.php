1b5553505ff80abce6b47d87b0192f0153eabfb5
Jennifer <jennifergapay@gmail.com>
2015-08-12 20:38:31 -0400

    twitterAPI: set up keys and test get request

--- twitter/twitter.php
+++ twitter/twitter.php
@@ -1,3 +1,17 @@
 <?php
 
+  session_start();
+
+  include("twitteroauth/twitteroauth.php");
+
+  $apikey="*******************";
+  $apisecret="*******************************";
+  $accesstoken="*****************************";
+  $accesssecret="*************************";
+
+  $tweets = 
+  $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2");
+
+  print_r($tweets);
+
 ?>
