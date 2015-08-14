af62109935edc8da0223b829939233ef59200d26
Jennifer <jennifergapay@gmail.com>
2015-08-13 22:11:32 -0400

    twitterAPI: print POST request

--- twitter/twitter.php
+++ twitter/twitter.php
@@ -11,13 +11,19 @@
 
   $connection = new TwitterOAuth($apikey, $apisecret, $accesstoken, $accesssecret);
 
-  $tweets = 
+
+//POST a tweet 
+  $response = $connection->post("https://api.twitter.com/1.1/statuses/update.json", array("status" =>"Testing an API"));
+
+  print_r($response);
   
-  $connection->post("https://api.twitter.com/1.1/statuses/update.json", array("status" =>"Testing an API"));
+//GETS latest tweets from a user
+   //$tweets = 
 
   //$connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=kellabyte&count=10");
 
-  foreach($tweets as $tweet) {
-    echo $tweet->text;
-    echo "<br />";
+  //foreach($tweets as $tweet) {
+    //echo $tweet->text;
+    //echo "<br />";
+
   }
