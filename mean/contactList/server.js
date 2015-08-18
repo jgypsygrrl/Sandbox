03ddd4005d141e19114c159eca7ab062604b92b5
Jennifer <jennifergapay@gmail.com>
2015-08-18 17:25:19 -0400

    MEANcontact: test if getting data from controller and server

--- mean/contactList/server.js
+++ mean/contactList/server.js
@@ -3,5 +3,30 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/contactList', function(req, res) {
+  console.log("I received a GET request")
+
+  person1 = {
+    name: 'Jay',
+    email: 'jay@email.com',
+    number: '(111)111-1111'
+  };
+
+  person2 = {
+    name: 'John',
+    email: 'john@email.com',
+    number: '(222)222-2222'
+  };
+
+  person3 = {
+    name: 'Jennifer',
+    email: 'jennifer@email.com',
+    number: '(333)333-3333'
+  };
+
+  var contactList = [person1, person2, person3];
+  res.json(contactList);
+});
+
 app.listen(3000);
 console.log("Server running on port 3000");
