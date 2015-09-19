1e5961964ae6a9f0ca42913935c03e3e7648b832
Jennifer <jennifergapay@gmail.com>
2015-09-18 23:16:17 -0400

    jsTennis: experiment ball speed, set up draw pieces on canvas

--- js-tennis-game/js/game.js
+++ js-tennis-game/js/game.js
@@ -1,2 +1,31 @@
 var canvas;
 var canvasContext;
+var ballX = 50;
+
+window.onload = function() {
+  canvas = document.getElementById('gameCanvas');
+  canvasContext = canvas.getContext('2d');
+
+  var framesPerSecond = 30;
+  setInterval(drawEverything, 1000 / framesPerSecond);
+}
+
+function callBoth() {
+  drawEverything();
+  moveEverything();
+}
+
+function moveEverything() {
+  ballX = ballX + 5;
+}
+
+function drawEverything() {
+
+
+  canvasContext.fillStyle = 'black';
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+  canvasContext.fillStyle = 'white';
+  canvasContext.fillRect(0, 210, 10, 100);
+  canvasContext.fillStyle = 'red';
+  canvasContext.fillRect(ballX, 100, 10, 10);
+}
