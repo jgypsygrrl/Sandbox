558d1fb035d68e839aeecd24193493a6d7e345bd
Jennifer <jennifergapay@gmail.com>
2015-09-19 00:03:02 -0400

    jsTennis: add ball to bounce vertically

--- js-tennis-game/js/game.js
+++ js-tennis-game/js/game.js
@@ -1,31 +1,51 @@
 var canvas;
 var canvasContext;
 var ballX = 50;
+var ballSpeedX = 10;
 
 window.onload = function() {
   canvas = document.getElementById('gameCanvas');
   canvasContext = canvas.getContext('2d');
 
   var framesPerSecond = 30;
-  setInterval(drawEverything, 1000 / framesPerSecond);
-}
-
-function callBoth() {
-  drawEverything();
-  moveEverything();
+  setInterval(function() {
+    drawEverything();
+    moveEverything();
+  }, 1000 / framesPerSecond);
 }
 
 function moveEverything() {
-  ballX = ballX + 5;
+  ballX += ballSpeedX;
+  if (ballX < 0) {
+    ballSpeedX = -ballSpeedX;
+  }
+  if (ballX > canvas.width) {
+    ballSpeedX = -ballSpeedX;
+  }
 }
 
 function drawEverything() {
 
+  //line below creates canvas
+  colorRect(0, 0, canvas.width, canvas.height, 'black');
 
-  canvasContext.fillStyle = 'black';
-  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-  canvasContext.fillStyle = 'white';
-  canvasContext.fillRect(0, 210, 10, 100);
-  canvasContext.fillStyle = 'red';
-  canvasContext.fillRect(ballX, 100, 10, 10);
+  //line below is the left paddle
+  colorRect(0, 210, 10, 100, 'white');
+
+  //line below is the ball
+  colorCircle(ballX, 150, 10, 'white');
+
+}
+
+function colorCircle(centerX, centerY, radius, drawColor) {
+  canvasContext.fillStyle = drawColor;
+  canvasContext.beginPath();
+  canvasContext.arc(centerX, centerY, 10, 0, Math.PI * 2, true);
+  canvasContext.fill();
+}
+
+function colorRect(leftX, topY, width, height, drawColor) {
+  canvasContext.fillStyle = drawColor;
+  canvasContext.fillRect(leftX, topY, width, height);
+
 }
