88940bfbc9072f8b069851b972aa9cb05e443fb2
Jennifer <jennifergapay@gmail.com>
2015-09-19 23:19:57 -0400

    jsTennis: Added left paddle movement & horizontal ball bounce

--- js-tennis-game/js/game.js
+++ js-tennis-game/js/game.js
@@ -1,7 +1,22 @@
 var canvas;
 var canvasContext;
 var ballX = 50;
+var ballY = 50;
 var ballSpeedX = 10;
+var ballSpeedY = 4;
+var paddle1Y = 250;
+var paddle1X = 40;
+
+function calculateMousePos(evt) {
+  var rect = canvas.getBoundingClientRect();
+  var root = document.documentElement;
+  var mouseX = evt.clientX - rect.left - root.scrollLeft;
+  var mouseY = evt.clientY - rect.top - root.scrollTop;
+  return {
+    x: mouseX,
+    y: mouseY
+  };
+}
 
 window.onload = function() {
   canvas = document.getElementById('gameCanvas');
@@ -12,7 +27,13 @@ window.onload = function() {
     drawEverything();
     moveEverything();
   }, 1000 / framesPerSecond);
-}
+
+  canvas.addEventListener('mousemove',
+    function(evt) {
+      var mousePos = calculateMousePos(evt);
+      paddle1Y = mousePos.y - (paddle1X / 2);
+    });
+};
 
 function moveEverything() {
   ballX += ballSpeedX;
@@ -22,25 +43,32 @@ function moveEverything() {
   if (ballX > canvas.width) {
     ballSpeedX = -ballSpeedX;
   }
+  ballY += ballSpeedY;
+  if (ballY < 0) {
+    ballSpeedY = -ballSpeedY;
+  }
+  if (ballY > canvas.height) {
+    ballSpeedY = -ballSpeedY;
+  }
 }
 
 function drawEverything() {
 
-  //line below creates canvas
+  //canvas
   colorRect(0, 0, canvas.width, canvas.height, 'black');
 
-  //line below is the left paddle
-  colorRect(0, 210, 10, 100, 'white');
+  //left paddle
+  colorRect(0, paddle1Y, 10, paddle1X, 'white');
 
-  //line below is the ball
-  colorCircle(ballX, 150, 10, 'white');
+  //the ball
+  colorCircle(ballX, ballY, 10, 'white');
 
 }
 
 function colorCircle(centerX, centerY, radius, drawColor) {
   canvasContext.fillStyle = drawColor;
   canvasContext.beginPath();
-  canvasContext.arc(centerX, centerY, 10, 0, Math.PI * 2, true);
+  canvasContext.arc(centerX, centerY, 5, 0, Math.PI * 2, true);
   canvasContext.fill();
 }
 
