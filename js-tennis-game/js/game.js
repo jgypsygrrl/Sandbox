bf4931157e3b5e9b5a5a1fc95701fc5a9805d6d6
Jennifer <jennifergapay@gmail.com>
2015-09-21 00:45:47 -0400

    jsTennis: Test and add AI to right paddle

--- js-tennis-game/js/game.js
+++ js-tennis-game/js/game.js
@@ -1,11 +1,16 @@
 var canvas;
 var canvasContext;
+
 var ballX = 50;
 var ballY = 50;
 var ballSpeedX = 10;
 var ballSpeedY = 4;
+
 var paddle1Y = 250;
-var paddle1X = 40;
+var paddle2Y = 250;
+var paddleHeight = 75;
+var paddleThickness = 10;
+
 
 function calculateMousePos(evt) {
   var rect = canvas.getBoundingClientRect();
@@ -28,25 +33,63 @@ window.onload = function() {
     moveEverything();
   }, 1000 / framesPerSecond);
 
+  //controls player paddle movements
   canvas.addEventListener('mousemove',
     function(evt) {
       var mousePos = calculateMousePos(evt);
-      paddle1Y = mousePos.y - (paddle1X / 2);
+      paddle1Y = mousePos.y - (paddleHeight / 2);
     });
 };
 
+function ballReset() {
+  ballSpeedX = -ballSpeedX;
+  ballX = canvas.width / 2;
+  ballY = canvas.height / 2;
+}
+
+//Right Paddle AI Movement
+function computerMovement() {
+  var paddle2YCenter = paddle2Y + (paddleHeight / 2);
+  if (paddle2YCenter < ballY - 35) {
+    paddle2Y += 6;
+  } else if (paddle2YCenter > ballY - 35) {
+    paddle2Y -= 6;
+  }
+}
+
 function moveEverything() {
+  computerMovement()
+
   ballX += ballSpeedX;
+  ballY += ballSpeedY;
+
+  //LEFT PADDLE
   if (ballX < 0) {
-    ballSpeedX = -ballSpeedX;
+    //if ball below top of the paddle & above the bottom of the paddle (top + height)
+    if (ballY > paddle1Y &&
+      ballY < paddle1Y + paddleHeight) {
+      //will flip ball's horizontal speed
+      ballSpeedX = -ballSpeedX;
+    } else {
+      ballReset();
+    }
   }
+  //RIGHT PADDLE
   if (ballX > canvas.width) {
-    ballSpeedX = -ballSpeedX;
+    if (ballY > paddle2Y &&
+      ballY < paddle2Y + paddleHeight) {
+      //will flip ball's horizontal speed
+      ballSpeedX = -ballSpeedX;
+    } else {
+      ballReset();
+    }
   }
+  //ball bounce off left side
   ballY += ballSpeedY;
   if (ballY < 0) {
     ballSpeedY = -ballSpeedY;
   }
+  //ball bounce vertically
   if (ballY > canvas.height) {
     ballSpeedY = -ballSpeedY;
   }
@@ -57,11 +100,16 @@ function drawEverything() {
   //canvas
   colorRect(0, 0, canvas.width, canvas.height, 'black');
 
-  //left paddle
-  colorRect(0, paddle1Y, 10, paddle1X, 'white');
+  //left player paddle
+  colorRect(0, paddle1Y, paddleThickness, paddleHeight, 'white');
+
+  //right computer paddle
+  colorRect(canvas.width - paddleThickness, paddle2Y, paddleThickness, paddleHeight, 'white');
 
   //the ball
   colorCircle(ballX, ballY, 10, 'white');
+
+  canvasContext.fillText("score", 100, 100);
 
 }
 
